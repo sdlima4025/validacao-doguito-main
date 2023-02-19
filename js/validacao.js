2495e9f9ad0ed1856106cddb178c9bb3dcc0aa1c
Sérgio Lima <sergiolimafg3@gmail.com>
2023-02-19 14:10:52 -0300

    correção do input date HTML

--- js/validacao.js
+++ js/validacao.js
@@ -1,23 +1,29 @@
-const dataNascimento = document.querySelector(`#nascimento`);
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
 
-dataNascimento.addEventListener('blur', (evento) => {
-    validaDataNascimento(evento.target);
-});
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+}
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
 
 function validaDataNascimento(input) {
-    const datarecebida = new Date(input.value);
-    let mensagem = '';
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
 
-    if(!maiorQue18(datarecebida)) {
-        mensagem = 'Você deve ser maior de 18 anos para se cadastrar!';
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
     }
 
-    input.setCustomValidity(mensagem);
+    input.setCustomValidity(mensagem)
 }
 
 function maiorQue18(data) {
-    const dataAtual = new Date();
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate() );
+    const dataAtual = new Date()
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
-    return dataMais18 <= dataAtual;
+    return dataMais18 <= dataAtual
 }
